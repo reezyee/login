3deba077815752d4735a9a648059c2ce84f0db6a
Reza Sapitra <113533664+reezyee@users.noreply.github.com>
2023-10-04 14:51:15 +0700

    Add files via upload

---
+++ jobsheet.js
@@ -0,0 +1,9 @@
+function login(userName, pass) {
+  console.log("---------------------")
+  console.log("        LOGIN        ")
+  console.log("---------------------")
+  console.log(`Username : ${userName}`)
+  console.log(`Passwod : ${pass}`)
+  console.log("---------------------")
+}
+login("reezyee", "****")
